5cfea5d80b2d6a42e6ec452378652ee58b38a6fb
Håvard H <33071768+haavardsjef@users.noreply.github.com>
2019-01-22 16:38:45 +0100

    Update Person.java

--- TDT4100_Objektorientert_Programmering/Oving2/Person.java
+++ TDT4100_Objektorientert_Programmering/Oving2/Person.java
@@ -7,6 +7,7 @@ public class Person {
     private String email;
     private Date birthday;
     private char gender;
+    final static String[] landskoder = {"ad", "ae", "af", "ag", "ai", "al", "am", "ao", "aq", "ar", "as", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "bj", "bl", "bm", "bn", "bo", "bq", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "cr", "cu", "cv", "cw", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "ee", "eg", "eh", "er", "es", "et", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gg", "gh", "gi", "gl", "gm", "gn", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "im", "in", "io", "iq", "ir", "is", "it", "je", "jm", "jo", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mf", "mg", "mh", "mk", "ml", "mm", "mn", "mo", "mp", "mq", "mr", "ms", "mt", "mu", "mv", "mw", "mx", "my", "mz", "na", "nc", "ne", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "ss", "st", "sv", "sx", "sy", "sz", "tc", "td", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tr", "tt", "tv", "tw", "tz", "ua", "ug", "um", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu", "wf", "ws", "ye", "yt", "za", "zm", "zw"};
     
     
     // Public methods
@@ -52,12 +53,22 @@ public class Person {
 	}
 	
 	//Sjekk at fornavn.etternavn kommer før @
-	if (email.substring(0, email.indexOf("@")).replaceAll(".", " ") != name) {
-	    throw new IllegalArgumentException("Eposten må være på formen fornavn.etternavn@domene.landskode");
+	if (!email.substring(0, email.indexOf("@")).replace('.', ' ').equals(this.name)) {
+	    throw new IllegalArgumentException("Eposten må inneholde fornavn og etternavn");
 	}
 	
 	//Sjekk at domene.landskode kommer etter @
-	if (email.substring(email.indexOf("@")).split(".") )
+	System.out.println(email.substring(email.indexOf("@")+1));
+	//String[] temp = email.substring(email.indexOf("@")+1).split(".");
+	String[] temp = "ntnu.no".split(".");
+	
+	System.out.println(temp.length);
+	if (temp.length != 2) {
+	    throw new IllegalArgumentException("Eposten må være på formen fornavn.etternavn@domene.landskode");
+	}
+	if (!lovliglandskode(temp[1])) {
+	    throw new IllegalArgumentException("Eposten må inneholde en lovlig landskode!");
+	}
         this.email = email;
     }
     
@@ -83,10 +94,23 @@ public class Person {
     }
     
     
+    private static boolean lovliglandskode(String kode) {
+	for (String landskode : landskoder) {
+	    if (kode == landskode) {
+		return true;
+	    }
+	}
+	return false;
+    }
+    
+    
+    
 public static void main(String[] args) {
     Person per1 = new Person();
-    per1.setName("Ola Nordmann");
-    System.out.println(per1.getName());
+    per1.setName("ola nordmann");
+   // System.out.println(per1.getName());
+    per1.setEmail("ola.nordmann@ntnu.no");
+    System.out.println(per1.getEmail());
     
 }
     
