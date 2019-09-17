bcec14ec163175c05c1fa4b88386ab21d0e36c67
Hjelmeseth <havhje@gmail.com>
2019-09-17 18:10:18 +0200

    commit

---
+++ TDT4120 - Algoritmer og datastrukturer/Oving3/bruteforce.jl
@@ -0,0 +1,6 @@
+# Hvis du fortsatt er med, godt jobbet! Som et lite pust i bakken skal du nå implementere en bruteforce løsning på problemet med å finne den minste avstanden mellom to punkter. Likevel skal du ikke være helt naiv og bruke n^2 sammenligninger, men heller n(n-1)/2.
+# 
+# Grunnen til at du skal implementere denne naive løsningen nå er at den skal brukes i basistilfellet i vår rekursive splitt og hersk løsning som vi skal implementere i siste oppgave.
+function bruteforce(x)
+
+end
